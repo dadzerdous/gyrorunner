e753598c1bd2e5f05cfedc75e4fbd576a0439432
dadzerdous <dadzaverous@gmail.com>
2026-01-19 20:15:04 -0500

    Update game.js

--- game.js
+++ game.js
@@ -1,1 +1,19 @@
-.
+let currentWave = 1;
+let gameState = 'WAVE'; // 'WAVE', 'UPGRADE', 'MAP'
+
+const roomTypes = {
+    COMBAT: { name: "Dark Woods", reward: "XP Boost" },
+    ELITE: { name: "Vampire Crypt", reward: "Artifact" },
+    REST: { name: "Blood Altar", reward: "Heal" }
+};
+
+function endWave() {
+    gameState = 'UPGRADE';
+    showUpgradeScreen();
+}
+
+function startNextRoom(choice) {
+    currentWave++;
+    gameState = 'WAVE';
+    spawnWave(currentWave, choice);
+}
